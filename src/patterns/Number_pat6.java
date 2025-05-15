439f42c50e1e4ae3a0b3be116ecef83447bed44c
“[Mounika <[bindumounika56@gmail.com]>
2025-05-15 11:50:56 +0530

    numerpat

---
+++ src/patterns/Number_pat6.java
@@ -0,0 +1,19 @@
+package patterns;
+
+public class Number_pat6 {
+	public static void main(String[] args) {
+		int sp=3;
+		for(int i=1;i<=4;i++) {
+			for(int j=0;j<sp;j++) {
+				System.out.print(" ");
+			}
+			sp--;
+			for(int k=1;k<=i;k++) {
+				System.out.print(i+" ");
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
